25cae0b3bc6facfcd38aac27a6c0131880aaa02f
lumidify <nobody@lumidify.org>
2020-03-30 20:11:42 +0200

    Add model for spinning wheel
    No, I'm not good at Blender/making models in general.

--- mods/lottcrafting/farming.lua
+++ mods/lottcrafting/farming.lua
@@ -1,61 +1,64 @@
 -- TODO: add different types of flour
 
-local register_machine = function(itemstring, name, instructions, x1, x2, x3, x4, src, dst)
-	minetest.register_node("lottcrafting:"..itemstring, {
-		description = name,
-		tiles = {"default_wood.png"},
-		groups = {cracky=2},
-		is_ground_content = false,
-		on_construct = function(pos)
-			local meta = minetest.get_meta(pos)
-			local inv = meta:get_inventory()
-			meta:set_string("infotext", name)
-			inv:set_size("src", 4)
-			inv:set_size("dst", 4)
-			meta:set_string("formspec", "size[8,10]"..
-				"label["..x1..",0;"..name.."]"..
-				"label["..x2..",1;"..instructions.."]"..
-				"label["..x3..",2;"..src.."]"..
-				"label["..x4..",2;"..dst.."]"..
-				"list[context;src;1.5,3;2,2]"..
-				"list[context;dst;4.5,3;2,2]"..
-				"list[current_player;main;0,6;8,4]"..
-				"listring[context;src]"..
-				"listring[current_player;main]"..
-				"listring[context;src]"..
-				"listring[context;dst]"..
-				"listring[current_player;main]"..
-				"background[-0.5,-0.5;9,11;gui_chestbg.png]"
-			)
-		end,
-		on_punch = function(pos, node, puncher, pointed_thing)
-			local meta = minetest.get_meta(pos)
-			local inv = meta:get_inventory()
-			local def = lottcrafting.get_craft_result(itemstring, 2, inv:get_list("src"), "shapeless")
-			if def and def.outputs then
-				lottcrafting.handle_craft(def, inv, "src", "dst", puncher)
-			else
-				minetest.chat_send_player(puncher:get_player_name(), "No craft could be found for your input item(s)!")
-			end
-		end,
-		allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-			if listname == "src" then return stack:get_count() end
-			return 0
-		end,
-		can_dig = function(pos, player)
-			local meta = minetest.get_meta(pos)
-			local inv = meta:get_inventory()
-			if not inv:is_empty("src") or not inv:is_empty("dst") then
-				return false
-			end
-			return true
-		end,
-	})
+local register_machine = function(itemstring, name, instructions, x1, x2, x3, x4, src, dst, def)
+	def.description = name
+	def.tiles = {"default_wood.png"}
+	def.groups = {cracky=2}
+	def.is_ground_content = false
+	def.on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		meta:set_string("infotext", name)
+		inv:set_size("src", 4)
+		inv:set_size("dst", 4)
+		meta:set_string("formspec", "size[8,10]"..
+			"label["..x1..",0;"..name.."]"..
+			"label["..x2..",1;"..instructions.."]"..
+			"label["..x3..",2;"..src.."]"..
+			"label["..x4..",2;"..dst.."]"..
+			"list[context;src;1.5,3;2,2]"..
+			"list[context;dst;4.5,3;2,2]"..
+			"list[current_player;main;0,6;8,4]"..
+			"listring[context;src]"..
+			"listring[current_player;main]"..
+			"listring[context;src]"..
+			"listring[context;dst]"..
+			"listring[current_player;main]"..
+			"background[-0.5,-0.5;9,11;gui_chestbg.png]"
+		)
+	end
+	def.on_punch = function(pos, node, puncher, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		local def = lottcrafting.get_craft_result(itemstring, 2, inv:get_list("src"), "shapeless")
+		if def and def.outputs then
+			lottcrafting.handle_craft(def, inv, "src", "dst", puncher)
+		else
+			minetest.chat_send_player(puncher:get_player_name(), "No craft could be found for your input item(s)!")
+		end
+	end
+	def.allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+		if listname == "src" then return stack:get_count() end
+		return 0
+	end
+	def.can_dig = function(pos, player)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		if not inv:is_empty("src") or not inv:is_empty("dst") then
+			return false
+		end
+		return true
+	end
+	minetest.register_node("lottcrafting:"..itemstring, def)
 end
 
-register_machine("mill", "Hand Mill", "Punch to grind items into flour.", 3.5, 2.2, 2, 5, "Source", "Flour")
-register_machine("spinning_wheel", "Spinning Wheel", "Punch to spin cotton into yarn.", 3, 2.2, 2, 5, "Cotton", "Yarn")
-register_machine("loom", "Loom", "Punch to weave yarn into fabric.", 3.5, 2, 2, 5, "Yarn", "Fabric")
+register_machine("mill", "Hand Mill", "Punch to grind items into flour.", 3.5, 2.2, 2, 5, "Source", "Flour", {})
+register_machine("spinning_wheel", "Spinning Wheel", "Punch to spin cotton into yarn.", 3, 2.2, 2, 5, "Cotton", "Yarn",
+	{drawtype = "mesh", mesh = "lottcrafting_spinning_wheel.obj",
+	selection_box = {type = "fixed", fixed = {-.2, -.5, -.5, .2, .5, .5}},
+	collision_box = {type = "fixed", fixed = {-.2, -.5, -.5, .2, .5, .5}},
+	paramtype2 = "facedir"})
+register_machine("loom", "Loom", "Punch to weave yarn into fabric.", 3.5, 2, 2, 5, "Yarn", "Fabric", {})
 
 lottcrafting.threshing_floor_formspec =
 "size[8,10]"..
